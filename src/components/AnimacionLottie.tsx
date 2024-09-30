1fbda5128962495597061fb4b89935277653f518
Jose Montes <jmontesc@comfenalco.com>
2024-09-29 20:01:27 -0500

    change favicon y hasrouter y otros mas

---
+++ src/components/AnimacionLottie.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+
+interface LottieAnimationProps {
+  animationData: object;  // Definimos el tipo de animationData como un objeto (puedes ser más específico si tienes un tipo concreto)
+  height?: number;        // Opcional, con valor por defecto
+  width?: number;         // Opcional, con valor por defecto
+  loop?: boolean;         // Opcional, con valor por defecto
+  autoplay?: boolean;     // Opcional, con valor por defecto
+}
+
+const LottieAnimation: React.FC<LottieAnimationProps> = ({
+  animationData,
+
+  loop = true,
+  autoplay = true,
+}) => {
+  const defaultOptions = {
+    loop: loop,
+    autoplay: autoplay,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return <Lottie options={defaultOptions}  />;
+};
+
+export default LottieAnimation;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PortadaPasaporte from "../components/Portada";
+import LottieAnimation from "../components/AnimacionLottie";
 import { useNavigate } from "react-router-dom";
+import ImageGallery from "../components/Galeria";
+import Avion from "../assets/animations/mapa1.json";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
@@ -9,8 +12,16 @@ const Home: React.FC = () => {
     navigate("/pasaporte"); // Cambia '/ruta-deseada' por la ruta a la que quieras navegar
   };
   return (
-    <div>
-      <PortadaPasaporte></PortadaPasaporte>
+    <div className="m-10 border-xl">
+      <div>
+        <p className="crimson-text ">EL VIAJE DE NUESTRAS VIDAS...</p>
+      </div>
+      <div>
+        <LottieAnimation animationData={Avion} loop={true} autoplay={true} />
+      </div>
+
+      <p className="crimson-text ">comienza aquí</p>
+      <button className="btn">VER</button>
     </div>
   );
 };
